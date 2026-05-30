10ce92c19c4b7f94e73b99268f2c10e74467d4cd
Geirel <Geirel@DESKTOP-5H54R8H>
2026-05-29 18:00:33 -0600

    Clases Persona y Direccion (Variables, constructor, this, get y set)

--- src/Direccion.java
+++ src/Direccion.java
@@ -9,4 +9,32 @@
  */
 public class Direccion {
     
+    //Variables
+    private String provincia;
+    private String cuidad;
+
+    //Constructor
+    public Direccion(String provincia, String cuidad) {
+        this.provincia = provincia;
+        this.cuidad = cuidad;
+    }
+    
+    //Getter y Setter
+    public String getProvincia() {
+        return provincia;
+    }
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+    public String getCuidad() {
+        return cuidad;
+    }
+    public void setCuidad(String cuidad) {
+        this.cuidad = cuidad;
+    }
+    
+    
+    
+    
+    
 }
--- src/Persona.java
+++ src/Persona.java
@@ -9,4 +9,38 @@
  */
 public class Persona {
     
+    //Variables privadas
+    private String nombre;
+    private String identificacion;
+    private Direccion direccion;
+    
+    //Variable publica y final, no cambia
+    public final String PAIS = "Costa Rica";
+    
+    //Constructor
+    public Persona (String nombre, String identificacion, Direccion direccion) {
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+        this.direccion = direccion;
+    }
+    
+    //Getter y Setter
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getIdentificacion() {
+        return identificacion;
+    }
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+    public Direccion getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }    
 }
