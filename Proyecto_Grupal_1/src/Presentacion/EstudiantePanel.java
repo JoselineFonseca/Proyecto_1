a5cae378707bf72b8a25e2404cf270602a3d21b9
JoselineFonseca <fonsecajoseline2@gmail.com>
2026-06-18 16:09:49 -0600

    Remove student registration button and related code

--- Proyecto_Grupal_1/src/Presentacion/EstudiantePanel.java
+++ Proyecto_Grupal_1/src/Presentacion/EstudiantePanel.java
@@ -7,29 +7,8 @@ package Presentacion;
  * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
  * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
  */
-import javax.swing.*;
-import Negocio.EstudianteNegocio;
-import Modelo.Estudiante;
-import Excepciones.*;
+
 
 public class EstudiantePanel extends JPanel {
-    private EstudianteNegocio negocio= new EstudianteNegocio ();
-    public EstudiantePanel (){
-        
-        //boton para registar estudiante
-        JButton btnRegistrar = new JButton ("Registrar");
-        btnRegistrar.addActionListener (e -> {
-            try {
-                Estudiante est = new Estudiante (
-                        1, 20,true ,"M","juan@correo.com", "Juan Pérez","Ingenería","88888888", "Ninguna"
-                );
-                negocio.registrar(est);
-                JOptionPane.showMessageDialog(this,"Estudiante registrado correctamente");
-            }catch (DatoInvalidoException | RegistrosDuplicadosException ex){
-                JOptionPane.showMessageDialog(this, ex.getMessage());
-            }
-        });
-        //agregar el boton al panel
-        add(btnRegistrar);
-}
+    
 }
