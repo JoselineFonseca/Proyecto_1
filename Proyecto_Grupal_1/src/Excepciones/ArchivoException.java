95800824dfe90c74cd42723e70e3de057b59f45e
fiore <fiore@fiorela>
2026-06-21 23:35:35 -0600

    Cree archivo exception

---
+++ Proyecto_Grupal_1/src/Excepciones/ArchivoException.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Excepciones;
+
+/**
+ *
+ * @author fiore
+ */
+public class ArchivoException extends Exception{
+    
+    public ArchivoException(String mensaje) {
+        super (mensaje);
+    }
+    public ArchivoException(String mensaje, Throwable causa){
+        super(mensaje, causa);
+    }
+}
