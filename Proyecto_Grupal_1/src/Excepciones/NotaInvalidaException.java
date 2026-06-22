721a898fc28ce18107c051f52fec2a1246dbb46c
fiore <fiore@fiorela>
2026-06-21 23:38:19 -0600

    Cree archivo NotaInvalidaException

---
+++ Proyecto_Grupal_1/src/Excepciones/NotaInvalidaException.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Excepciones;
+
+/**
+ *
+ * @author fiore
+ */
+public class NotaInvalidaException extends Exception {
+    
+    public NotaInvalidaException (String mensaje){
+        super(mensaje);
+    }
+}
+
+
